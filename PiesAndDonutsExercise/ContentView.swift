df9e1ad74b96ecbb2118b19918408117efded06b
Youzhou Zhang <czhang@lcs.on.ca>
2024-11-09 11:15:39 -0500

    made a simple donut chart that shows a made up data of students in each house in LCS

--- PiesAndDonutsExercise/ContentView.swift
+++ PiesAndDonutsExercise/ContentView.swift
@@ -6,16 +6,45 @@
 //
 
 import SwiftUI
+import Charts
 
 struct ContentView: View {
+    private var percentageStudentsByHouse = [
+        (name: "Matthews", count: 120),
+        (name: "Ross", count: 202),
+        (name: "Moddie", count: 678),
+        (name: "Parent", count: 776),
+        (name: "Ondaatje", count: 432),
+        (name: "Upland", count: 423),
+        (name: "Colebrook", count: 543),
+        (name: "Cooper", count: 722),
+        (name: "Memorial", count: 636),
+        (name: "Grove", count: 456),
+        (name: "Rashleigh", count: 647),
+        (name: "Ryder", count: 167)
+    ]
+    @State private var selectedCount: Double?
+    @State private var selectedSector: String?
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        Chart {
+            ForEach(percentageStudentsByHouse, id: \.name) { student in
+
+                SectorMark(
+                    angle: .value("Studets", student.count),
+                    innerRadius: .ratio(0.65),
+                    angularInset: 2.0
+                )
+                .foregroundStyle(by: .value("Type", student.name))
+                .cornerRadius(10.0)
+                .annotation(position: .overlay) {
+                    Text("\(student.count)")
+                        .font(.headline)
+                        .foregroundStyle(.white)
+                }
+                .opacity(selectedSector == nil ? 1.0 : (selectedSector == student.name ? 1.0 : 0.5))
+            }
         }
-        .padding()
+        .frame(height: 500)
     }
 }
 
