e4d2ddb3f878973a600839e194f8ac8cef3340c6
Youzhou Zhang <czhang@lcs.on.ca>
2024-11-08 20:06:29 -0500

    Initial Commit

---
+++ PiesAndDonutsExercise/PiesAndDonutsExerciseApp.swift
@@ -0,0 +1,17 @@
+//
+//  PiesAndDonutsExerciseApp.swift
+//  PiesAndDonutsExercise
+//
+//  Created by Yuzhou Zhang on 2024-11-08.
+//
+
+import SwiftUI
+
+@main
+struct PiesAndDonutsExerciseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
